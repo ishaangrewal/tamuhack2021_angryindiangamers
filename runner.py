84b1cbb7e304b41bf3e4dcf9017446c406a354f7
Samyukt Metta <samyukt30@gmail.com>
2021-01-31 00:56:22 -0600

    New changes

--- runner.py
+++ runner.py
@@ -1,6 +1,18 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 from tamuhack2021_angryindiangamers import script
+
 app = Flask(__name__)
+
+weight = None
+height = None
+age = None
+sex = None
+cholesterol = None
+excercise = None
+glucose = None
+diastolic = None
+systolic = None
+
 
 @app.route("/")
 def home():
@@ -10,9 +22,12 @@ def home():
 def about():
     return "About"
 
-@app.route("/predict/")
+@app.route("/predict/", methods=["POST", "GET"])
 def predict():
-    return "Predict"
+    if request.method == "POST":
+
+    else:
+        render_template("predict.html")
 
 
 if __name__ == "__main__":
